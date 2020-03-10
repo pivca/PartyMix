43c7cc86d0b7e3b1b093f19f11441a1b26111e9f
PIVAN <ivanstojkovice11@gmail.com>
2020-03-10 21:14:52 +0100

    struktura projekta i app.js a

---
+++ app.js
@@ -0,0 +1,34 @@
+const express =require("express");
+var MongoClient = require('mongodb').MongoClient;
+
+
+
+var url = "mongodb+srv://Ivan:javolimtatu2@partymode-hxqit.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+var app = express();
+app.set("view engine", "ejs");
+app.use(express.static((__dirname, './css')));
+
+
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+app.get("/insert",(req,res)=>{
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("Party");
+        var myobj = { name: "Company Inc", address: "Highway 37" };
+        dbo.collection("Party").insertOne(myobj, function(err, res) {
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        });
+      });
+      
+})
+
+
+app.listen(3000,()=>{
+    console.log("startovan server");
+})
